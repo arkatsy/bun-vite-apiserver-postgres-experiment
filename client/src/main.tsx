cb84f98a1ad63e31ba6df568000844741781697a
Argyris <arkatsy@gmail.com>
2024-05-09 00:46:36 +0300

    feat: init w/ deps

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Root from "./root.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+const queryClient = new QueryClient();
+
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new ReferenceError("Root element not found");
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
---
+++ client/src/root.tsx
@@ -0,0 +1,3 @@
+export default function Root() {
+  return <div className="bg-red-400">Hello World</div>;
+}
